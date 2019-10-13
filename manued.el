6bef458b59d9d897b0a3ad45e8e4aac1a4ed8636
Jonas Bernoulli <jonas@bernoul.li>
2019-10-13 14:55:35 +0200

    Fix typos in permission statement

--- manued.el
+++ manued.el
@@ -13,13 +13,15 @@
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either versions 2, or (at your option)
+;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; This program is distributed in the hope that it will be useful
+;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
 ;; see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
